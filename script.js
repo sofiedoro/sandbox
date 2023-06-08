abfe4483ee2d606032713d543a601129db2f9971
Biebie <114484426+sofiedoro@users.noreply.github.com>
2023-06-09 00:50:59 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,4 +1,10 @@
-
+const groceries = document.getElementsByClassName
+("groceries")[0];
+const pencil = document.getElementById("pencil");
+const allItems = document.getElementById
+("allItems");
+const userInput = document.getElementById 
+("userInput");
 
 pencil.addEventListener("click", function(){
     allItems.innerHTML = "";
@@ -22,26 +28,3 @@ allItems.insertAdjacentElement("beforeend", h2);
 
 userInput.value = "";
 }
-let slideIndex = 1;
-showSlides(slideIndex);
-
-function plusSlides(n) {
-  showSlides(slideIndex += n);
-}
-
-function currentSlide(n) {
-  showSlides(slideIndex = n);
-}
-
-function showSlides(n) {
-  let i;
-  let slides = document.getElementsByClassName("mySlides");
-  let dots = document.getElementsByClassName("demo");
-  let captionText = document.getElementById("caption");
-  if (n > slides.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
-  }
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
